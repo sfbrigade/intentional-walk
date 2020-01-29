84c511427667b7125bf40615dffdb3e0d1f35c14
Francis Li <mail@francisli.com>
2020-01-29 15:13:27 -0800

    POC test, react-native-fitness for querying step data

--- IntentionalWalkApp/App.js
+++ IntentionalWalkApp/App.js
@@ -4,7 +4,7 @@
  * Alexander Huesing
  */
 
-import React, { useState } from 'react';
+import * as React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -19,49 +19,88 @@ import {
 //Found a progress bar to steal
 import ProgressBar from 'react-native-progress/Bar';
 
+import Fitness from '@ovalmoney/react-native-fitness';
 
-const  App:() => React$Node = () => {
+
+
+class App extends React.Component {
   //is this where I would put the const data?
   //will default state of empty string be appropriate here?
   //TODO: onPressButton
-  const [buttonPress, steps] = useState('');
+  // const [buttonPress, steps] = useState('');
+
+  componentDidMount() {
+    Fitness.isAuthorized()
+      .then((authorized) => {
+        console.log("fitness authorized:", authorized);
+        if (!authorized) {
+          Fitness.requestPermissions()
+            .then((authorized) => {
+              console.log("fitness authorized by user:", authorized);
+              this.getSteps();
+            })
+            .catch((error) => {
+              console.err("error requesting fitness authorization", error);
+            });
+        } else {
+          this.getSteps();
+        }
+      })
+      .catch((error) => {
+        //Do something
+        console.err("error querying fitness authorization", error);
+      });
+  }
+
+  getSteps() {
+    Fitness.getSteps({
+      startDate: '2020-01-28',
+      endDate: '2020-01-29',
+      interval: 'days'
+    }).then((steps) => {
+      console.log("steps", steps);
+    }).catch((error) => {
+      console.log("error requesting steps", error);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.container}>
+        {/* RIP ScrollView*/}
+
+            {/* maybe keep this insane HermesInternal thing in for now but I have no idea what its doing */}
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+
+          <View style={styles.progressView}>
+
+            {/* TODO: How to increment progress bar?
+            -progress should equal "steps / step goal" or something
+            -get feedback on width*/}
+            <ProgressBar progress={.9} width={200} />
+          </View>
+
+          <View style={styles.sectionContainer}>
+            <Button title="Step" style = {styles.stepButton}/>
+          </View>
 
 
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-      {/* RIP ScrollView*/}
-          
-          {/* maybe keep this insane HermesInternal thing in for now but I have no idea what its doing */}
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-        
-        <View style={styles.progressView}>
-        
-          {/* TODO: How to increment progress bar? 
-          -progress should equal "steps / step goal" or something
-          -get feedback on width*/}
-          <ProgressBar progress={.9} width={200} />
-        </View>
-        
-        <View style={styles.sectionContainer}>
-          <Button title="Step" style = {styles.stepButton}/>
-        </View>
+          <View style={styles.sectionContainer}>
+            <TextInput placeholder="Enter email" style = {styles.userInputText} />
+            <TextInput placeholder="Set Password" style = {styles.userInputText}/>
+          </View>
 
 
-        <View style={styles.sectionContainer}>
-          <TextInput placeholder="Enter email" style = {styles.userInputText} />
-          <TextInput placeholder="Set Password" style = {styles.userInputText}/>
-        </View>
-
-
-      </SafeAreaView>
-    </>
-  );
+        </SafeAreaView>
+      </>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -69,14 +108,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  
+
   stepButton: {
-    justifyContent: 'center', 
+    justifyContent: 'center',
     paddingTop: 100,
   },
 
   userInputText: {
-    borderBottomWidth: 1, 
+    borderBottomWidth: 1,
     borderBottomColor: 'black',
   },
 
@@ -98,7 +137,7 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
     justifyContent: "center",
-    
+
   },
   sectionTitle: {
     fontSize: 24,
