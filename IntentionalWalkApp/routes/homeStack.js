f5493af738ad95fb39a6992405ae2bcc7ae5cf5c
Johnson <johnsonphan95@gmail.com>
2020-02-01 19:16:24 -0800

    Add welcome screen

--- IntentionalWalkApp/routes/homeStack.js
+++ IntentionalWalkApp/routes/homeStack.js
@@ -1,10 +1,14 @@
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import Welcome from '../screens/welcome';
+import SignUp from '../screens/signup';
 
 const screens = {
   Welcome: {
     screen: Welcome,
+  },
+  SignUp: {
+    screen: SignUp,
   },
 };
 
--- IntentionalWalkApp/screens/welcome.js
+++ IntentionalWalkApp/screens/welcome.js
@@ -2,9 +2,99 @@ import React, {useState} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
 export default function Welcome({navigation}) {
+  const [language, setLanguage] = useState(null);
+  const [startText, setStartText] = useState(null);
+
+  const selectLanguage = lang => {
+    let str;
+    if (lang === 'english') {
+      str = 'START';
+    }
+    if (lang === 'spanish') {
+      str = 'COMENZAR';
+    }
+    if (lang === 'chinese') {
+      str = '开始';
+    }
+    setLanguage(lang);
+    setStartText(str);
+  };
+
+  const continuePress = () => {
+    navigation.navigate('SignUp');
+  };
+
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <Text style={{fontSize: 40, bottom: 100}}>Welcome!</Text>
+      <Text style={{fontSize: 25, bottom: 100}}>Select a language</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => selectLanguage('english')}>
+        <Text style={styles.text}>ENGLISH</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => selectLanguage('spanish')}>
+        <Text style={styles.text}>ESPAÑOL</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => selectLanguage('chinese')}>
+        <Text style={styles.text}>中文</Text>
+      </TouchableOpacity>
+
+      {!language ? null : (
+        <TouchableOpacity style={styles.startButton} onPress={continuePress}>
+          <Text style={styles.text}>{startText}</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'limegreen',
+    borderRadius: 7,
+    height: 50,
+    width: 190,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    bottom: 50,
+  },
+  buttonPress: {
+    backgroundColor: 'purple',
+    borderRadius: 7,
+    height: 50,
+    width: 190,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  startButton: {
+    position: 'absolute',
+    backgroundColor: 'purple',
+    borderRadius: 7,
+    height: 50,
+    width: 190,
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: 100,
+  },
+  none: {
+    display: 'none',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+    fontFamily: 'Arial',
+  },
+});
