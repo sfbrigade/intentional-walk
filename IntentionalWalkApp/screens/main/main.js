7cb8e77c8fed3f05f15e353cbb19c96658d93e67
Francis Li <mail@francisli.com>
2020-02-09 18:53:28 -0800

    Back to function syntax

--- IntentionalWalkApp/screens/main/main.js
+++ IntentionalWalkApp/screens/main/main.js
@@ -1,17 +1,14 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
-export default class MainScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props.navigation.navigate('OnboardingStack');
-  }
+export default function MainScreen({navigation}) {
+  useEffect(() => {
+    navigation.navigate('OnboardingStack');
+  });
 
-  render() {
-    return (
-      <View>
-        <Text>Main!</Text>
-      </View>
-    );
-  }
+  return (
+    <View>
+      <Text>Main!</Text>
+    </View>
+  );
 }
