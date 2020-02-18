8827689adc8c73e257098ed7de3a77b9a59186a0
Francis Li <mail@francisli.com>
2020-02-17 17:48:58 -0800

    Ensure logo centered, also on Android

--- IntentionalWalkApp/routes/onboardingStack.js
+++ IntentionalWalkApp/routes/onboardingStack.js
@@ -7,7 +7,7 @@ const Stack = createStackNavigator();
 
 export default function OnboardingStack() {
   return (
-    <Stack.Navigator screenOptions={{title: null, headerTitle: props => <Logo />}}>
+    <Stack.Navigator screenOptions={{title: null, headerTitle: props => <Logo />, headerTitleContainerStyle: {left: 0, width: '100%'}}}>
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen name="Info" component={InfoScreen} />
