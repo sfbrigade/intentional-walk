b152340a1159a5ee151365408d6ea73c3f72aa43
Francis Li <mail@francisli.com>
2020-02-19 17:36:16 -0800

    Quick re-style of last onboarding screen

--- IntentionalWalkApp/screens/onboarding/info.js
+++ IntentionalWalkApp/screens/onboarding/info.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {
+  SafeAreaView,
+  ScrollView,
   StyleSheet,
   View,
   Text,
@@ -8,7 +10,8 @@ import {
   Image,
   Platform,
 } from 'react-native';
-import {CheckBox} from 'react-native-elements';
+import {Button} from '../../components';
+import {Colors, GlobalStyles} from '../../styles';
 import Fitness from '@ovalmoney/react-native-fitness';
 
 export default function InfoScreen({navigation}) {
@@ -30,47 +33,50 @@ export default function InfoScreen({navigation}) {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={{marginTop: 50, fontSize: 26}}>You're Signed Up!</Text>
-      <View style={{flex: 1, margin: 20, alignSelf: 'stretch'}}>
-        <Text style={{alignSelf: 'flex-start', margin: 10, fontSize: 16}}>
-          From here, participation is easy:
-        </Text>
-        <View style={[styles.box, {backgroundColor: 'turquoise'}]}>
-          <Text style={[styles.whiteText, {fontSize: 24}]}>Walk!</Text>
-          <Text style={styles.whiteText}>
-            This all will count your steps taken each day, just carry your phone
-            with you when you're walking.
-          </Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.content}>
+          <Text style={GlobalStyles.h1}>You're signed up!</Text>
+          <View style={{flex: 1, alignSelf: 'stretch'}}>
+            <Text style={{textAlign: 'center', marginBottom: 16, fontSize: 17, color: Colors.primary.gray2}}>
+              From here, participation is easy:
+            </Text>
+            <View style={[styles.box, {backgroundColor: 'turquoise'}]}>
+              <Text style={[styles.whiteText, {fontSize: 24}]}>Walk!</Text>
+              <Text style={styles.whiteText}>
+                This all will count your steps taken each day, just carry your phone
+                with you when you're walking.
+              </Text>
+            </View>
+            <View style={[styles.box, {backgroundColor: 'purple'}]}>
+              <Text style={[styles.whiteText, {fontSize: 24}]}>Record!</Text>
+              <Text style={styles.whiteText}>
+                Use the record function to keep track of individual walks that you
+                take. Whether you're walking for exercise or just on your way to
+                work, it all counts!
+              </Text>
+            </View>
+            <View style={[styles.box, {backgroundColor: 'orange'}]}>
+              <Text style={[styles.whiteText, {fontSize: 24}]}>Win!</Text>
+              <Text style={styles.whiteText}>
+                Top walkers will be notified of their prize winnings over email.
+                Prizes include tickets, merchandise and a special Grand Prize!!
+              </Text>
+            </View>
+          </View>
+          <Button style={styles.button} onPress={startPressed}>Next</Button>
         </View>
-        <View style={[styles.box, {backgroundColor: 'purple'}]}>
-          <Text style={[styles.whiteText, {fontSize: 24}]}>Record!</Text>
-          <Text style={styles.whiteText}>
-            Use the record function to keep track of individual walks that you
-            take. Whether you're walking for exercise or just on your way to
-            work, it all counts!
-          </Text>
-        </View>
-        <View style={[styles.box, {backgroundColor: 'orange'}]}>
-          <Text style={[styles.whiteText, {fontSize: 24}]}>Win!</Text>
-          <Text style={styles.whiteText}>
-            Top walkers will be notified of their prize winnings over email.
-            Prizes include tickets, merchandise and a special Grand Prize!!
-          </Text>
-        </View>
-      </View>
-      <TouchableOpacity style={styles.button}>
-        <Text style={{color: 'white', fontWeight: 'bold', fontSize: 24}} onPress={startPressed}>
-          START
-        </Text>
-      </TouchableOpacity>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  content: {
+    ...GlobalStyles.content,
     alignItems: 'center',
   },
   whiteText: {
@@ -78,20 +84,13 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   box: {
-    borderRadius: 4,
-    height: 130,
-    margin: 10,
-    padding: 10,
+    ...GlobalStyles.rounded,
+    height: 140,
+    padding: 16,
+    marginBottom: 16,
   },
   button: {
-    position: 'absolute',
-    backgroundColor: 'purple',
-    borderRadius: 7,
-    height: 60,
-    width: 200,
-    alignItems: 'center',
-    justifyContent: 'center',
-    bottom: 100,
-    color: 'white',
+    width: 180,
+    height: 48,
   },
 });
