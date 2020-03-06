d1d112edc5e7e535c837e525d0b750d274f5b1a6
John Lee <drumwolf@gmail.com>
2020-03-06 05:47:24 -0800

    replaced View/Text components with Button inside LinkButton component

--- components/linkButton.js
+++ components/linkButton.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import {Linking, StyleSheet, Text, View} from 'react-native';
+import {Button} from './index';
 import {Colors, GlobalStyles} from '../styles';
 
 export default function LinkButton({title, url}) {
   return (
-    <View style={styles.content}>
-      <Text style={styles.text} onPress={() => {Linking.openURL(url)} }>{title}</Text>
-    </View>
+    <Button style={styles.content} onPress={() => {Linking.openURL(url)} }>{title}</Button>
   );
 }
 
 const styles = StyleSheet.create({
   content: {
     ...GlobalStyles.content,
-    ...GlobalStyles.rounded,
-    alignItems: 'center',
-    backgroundColor: Colors.primary.purple,
-    justifyContent: 'center',
     marginVertical: 8,
     height: 48,
-    textAlign: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 24,
-    textAlign: 'center'
   }
 });
