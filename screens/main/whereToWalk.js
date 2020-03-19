3dfa1fe737c811a93905c27a4b79b2c472a2045a
drumwolf <drumwolf@gmail.com>
2020-03-19 10:20:40 -0700

    first draft of Translations project: apply TL strings to whereToWalk

--- screens/main/whereToWalk.js
+++ screens/main/whereToWalk.js
@@ -12,12 +12,13 @@ import {
 } from 'react-native';
 import {Button, InfoBox, LinkButton, PageTitle} from '../../components';
 import {Colors, GlobalStyles} from '../../styles';
+import {TLWhereToWalk} from '../../translations/';
 
 const links = [
-  { title: "Parks & Rec Centers", url: "https://sfrecpark.org/facilities" },
-  { title: "Hiking Trails in SF", url: "https://sfrecpark.org/448/Trails-Hikes" },
-  { title: "Guided Walks", url: "https://sfrecpark.org/1244/Healthy-Parks-Healthy-People" },
-  { title: "Exercise & Fitness Activities", url: "https://apm.activecommunities.com/sfrecpark/Activity_Search?detailskeyword=&IsAdvanced=True&ddlSortBy=Activity+name&ActivityCategoryID=29&DaysOfWeek=0000000&SearchFor=2&SearchLevelID=2&maxAge=100&NumberOfItemsPerPage=20&IsSearch=true" }
+  { title: TLWhereToWalk.parks, url: "https://sfrecpark.org/facilities" },
+  { title: TLWhereToWalk.trails, url: "https://sfrecpark.org/448/Trails-Hikes" },
+  { title: TLWhereToWalk.walks, url: "https://sfrecpark.org/1244/Healthy-Parks-Healthy-People" },
+  { title: TLWhereToWalk.exercise, url: "https://apm.activecommunities.com/sfrecpark/Activity_Search?detailskeyword=&IsAdvanced=True&ddlSortBy=Activity+name&ActivityCategoryID=29&DaysOfWeek=0000000&SearchFor=2&SearchLevelID=2&maxAge=100&NumberOfItemsPerPage=20&IsSearch=true" }
 ]
 
 export default function WhereToWalkScreen({navigation}) {
@@ -26,7 +27,7 @@ export default function WhereToWalkScreen({navigation}) {
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={GlobalStyles.content}>
-          <PageTitle title="Where to Walk" />
+          <PageTitle title={TLWhereToWalk.title} />
           <Image source={require('../../assets/sfrecparks_logo.png')} style={styles.image} />
           { linkBoxes }
         </View>
---
+++ translations/whereToWalk.js
@@ -0,0 +1,27 @@
+import LocalizedStrings from 'react-native-localization';
+
+let TLWhereToWalk = new LocalizedStrings({
+ "en-US":{
+   title: "Where to Walk",
+   parks: "Parks & Rec Centers",
+   trails: "Hiking Trails in SF",
+   walks:  "Guided Walks",
+   exercise: "Exercise & Fitness Activities"
+ },
+ es:{
+   title: "Where to Walk",
+   parks: "Parks & Rec Centers",
+   trails: "Hiking Trails in SF",
+   walks:  "Guided Walks",
+   exercise: "Exercise & Fitness Activities"
+ },
+ cn: {
+   title: "Where to Walk",
+   parks: "Parks & Rec Centers",
+   trails: "Hiking Trails in SF",
+   walks:  "Guided Walks",
+   exercise: "Exercise & Fitness Activities"
+ }
+});
+
+export default TLWhereToWalk;
