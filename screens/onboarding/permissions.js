72929753dfb3d8dc3e86a97a76e87178543fd834
drumwolf <drumwolf@gmail.com>
2020-03-19 19:48:45 -0700

    added translations to permissions page

--- screens/onboarding/permissions.js
+++ screens/onboarding/permissions.js
@@ -13,6 +13,7 @@ import {
 import {Button, InfoBox} from '../../components';
 import {Colors, GlobalStyles} from '../../styles';
 import Fitness from '@ovalmoney/react-native-fitness';
+import {TLPermissions} from '../../translations';
 
 export default function InfoScreen({navigation}) {
 
@@ -36,28 +37,28 @@ export default function InfoScreen({navigation}) {
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.content}>
-        <Text style={GlobalStyles.h1}>Things to know.</Text>
+        <Text style={GlobalStyles.h1}>{TLPermissions.title}</Text>
         <View style={{flex: 1, alignSelf: 'stretch'}}>
-          <Text style={styles.subtitle}>Take a look at this information before you get started.</Text>
+          <Text style={styles.subtitle}>{TLPermissions.subtitle}</Text>
             <InfoBox icon="settings"
                      iconSize={64}
                      iconColor={Colors.primary.purple}>
-             The iWalk App uses {Platform.select({ios: 'Apple Health', android: 'Google Fit'})} to keep track of your steps. In the next screens, you will be asked to allow iWalk to use this information.
+             {TLPermissions.settingsText}
             </InfoBox>
             <InfoBox icon="lock"
                      iconSize={64}
                      iconColor={Colors.secondary.blue}>
-              To win a prize through Intentional Walk, you must allow the iWalk App to use {Platform.select({ios: 'Apple Health', android: 'Google Fit'})} information. All information will be kept private and used only for the Intentional Walk program.
+              {TLPermissions.prizeText}
             </InfoBox>
             { Platform.OS == 'android' ? (
               <InfoBox icon="account-circle"
                        iconSize={64}
                        iconColor={Colors.primary.darkGreen}>
-                Google Fit requires a Google account, if you do not have one, learn how to get one here.
+                {TLPermissions.googleText}
               </InfoBox>
             ) : null }
           </View>
-          <Button style={styles.button} onPress={onNextPress}>Next</Button>
+          <Button style={styles.button} onPress={onNextPress}>{TLPermissions.next}</Button>
         </View>
       </ScrollView>
     </SafeAreaView>
--- translations/index.js
+++ translations/index.js
@@ -1,4 +1,5 @@
 export TLHome from './home';
 export TLInformation from './information';
+export TLPermissions from './permissions';
 export TLRecordedWalks from './recordedWalks';
 export TLWhereToWalk from './whereToWalk';
