5350dfa71ac47384c30915dc7d68aaf451e09dae
Francis Li <mail@francisli.com>
2021-08-05 14:05:19 -0700

    [#176] Update google signin branding (#192)
    * use google sign-in logo for branding requirements
    
    * Android build 49
    
    * remove files accidently committed by fastlane
    
    * Update button asset, increase size
    
    Co-authored-by: Jeanine Walters <jeaninesw@gmail.com>
    Co-authored-by: Francis Li <mail@francisli.com>

--- screens/onboarding/permissions.js
+++ screens/onboarding/permissions.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   Image,
   Linking,
@@ -11,11 +11,11 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import {Button, InfoBox, PaginationDots} from '../../components';
-import {Colors, GlobalStyles} from '../../styles';
-import {Fitness, Strings} from '../../lib';
+import { Button, InfoBox, PaginationDots } from '../../components';
+import { Colors, GlobalStyles } from '../../styles';
+import { Fitness, Strings } from '../../lib';
 
-export default function InfoScreen({navigation}) {
+export default function InfoScreen({ navigation }) {
 
   const onNextPress = () => {
     Fitness.requestPermissions().then((permitted) => {
@@ -29,33 +29,40 @@ export default function InfoScreen({navigation}) {
     <SafeAreaView style={GlobalStyles.container}>
       <ScrollView>
         <View style={styles.content}>
-        <Text style={GlobalStyles.h1}>{Strings.permissions.thingsToKnow}</Text>
-        <View style={{flex: 1, alignSelf: 'stretch'}}>
-          <Text style={styles.subtitle}>{Strings.permissions.takeALookText}</Text>
+          <Text style={GlobalStyles.h1}>{Strings.permissions.thingsToKnow}</Text>
+          <View style={{ flex: 1, alignSelf: 'stretch' }}>
+            <Text style={styles.subtitle}>{Strings.permissions.takeALookText}</Text>
             <InfoBox style={styles.infoBox}
-                     icon="settings"
-                     iconSize={64}
-                     iconColor={Colors.primary.lightGreen}
-                     iconStyle={{marginTop: 10}}>
-             {Strings.permissions.settingsText}
+              icon="settings"
+              iconSize={64}
+              iconColor={Colors.primary.lightGreen}
+              iconStyle={{ marginTop: 10 }}>
+              {Strings.permissions.settingsText}
             </InfoBox>
-            <InfoBox style={[styles.infoBox, Platform.select({ios: styles.infoBoxLast})]}
-                     icon="lock"
-                     iconSize={64}
-                     iconColor={Colors.secondary.blue}
-                     iconStyle={{marginTop: 20}}>
+            <InfoBox style={[styles.infoBox, Platform.select({ ios: styles.infoBoxLast })]}
+              icon="lock"
+              iconSize={64}
+              iconColor={Colors.secondary.blue}
+              iconStyle={{ marginTop: 20 }}>
               {Strings.permissions.prizeText}
             </InfoBox>
-            { Platform.OS == 'android' ? (
+            {Platform.OS == 'android' ? (
               <InfoBox style={[styles.infoBox, styles.infoBoxLast]}
-                       icon="account-circle"
-                       iconSize={64}
-                       iconColor={Colors.primary.darkGreen}>
+                icon="account-circle"
+                iconSize={64}
+                iconColor={Colors.primary.darkGreen}>
                 <Text onPress={() => Linking.openURL(`https://support.google.com/accounts/answer/27441?hl=${Strings.getLanguage()}`)}>{Strings.formatString(Strings.permissions.googleText, <Text style={styles.linkText}>{Strings.permissions.getOneHere}</Text>)}</Text>
               </InfoBox>
-            ) : null }
+            ) : null}
           </View>
-          <Button style={styles.button} onPress={onNextPress}>{Strings.common.next}</Button>
+          {Platform.OS == 'android' ?
+            (
+              <Button style={styles.googleButton} onPress={onNextPress}>
+                <Image style={styles.googleButton} source={require('../../assets/btn_google_signin_dark_normal_web.png')} />
+              </Button>
+            ) : (
+              <Button style={styles.button} onPress={onNextPress}>{Strings.common.next}</Button>
+            )}
           <PaginationDots currentPage={3} totalPages={3} />
         </View>
       </ScrollView>
@@ -86,6 +93,11 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
     color: Colors.primary.purple,
     fontWeight: 'bold',
+  },
+  googleButton: {
+    backgroundColor: Colors.primary.lightGray,
+    resizeMode: 'contain',
+    height: 62,
   },
   button: {
     width: 180,
