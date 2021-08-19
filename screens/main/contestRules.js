91321e61e39b240a0960145443f4762fa6d075bf
Jeanine Walters <jeaninesw@gmail.com>
2021-08-19 15:19:29 -0700

    1) updated contestRules text to use dynamic contest info from the server, 2) updated website link, 3) fixed a couple of typos.

--- screens/main/contestRules.js
+++ screens/main/contestRules.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Dimensions,
   Image,
@@ -15,12 +15,36 @@ import loadLocalResource from 'react-native-local-resource'
 import Autolink from 'react-native-autolink';
 import {Button, InfoBox, PageTitle} from '../../components';
 import {Colors, GlobalStyles} from '../../styles';
-import {Strings} from '../../lib';
+import {Realm, Strings} from '../../lib';
+import moment from 'moment';
+
 
 import ContestRules from '../../assets/contestRules';
 
 export default function ContestRulesScreen({navigation}) {
   const [text, setText] = useState();
+  const [contest, setContest] = useState(null);
+
+  useEffect(() => {
+    Realm.getContest().then(contest => setContest(contest ? contest.toObject() : null));
+  }, []);
+
+  let from = null, to = null, fromEn = null, toEn = null;
+  // English Contest Rules use long form of date (rangeTo format)
+  // Other languages use short form (dateSlash format)
+  if (contest) {
+    from = moment(contest.start).format(Strings.common.dateSlash);
+    to = moment(contest.end).format(Strings.common.dateSlash);
+    fromEn = moment(contest.start).format(Strings.common.rangeTo);
+    toEn = moment(contest.end).format(Strings.common.rangeTo);
+  } else {
+    // default value just in case contest is unavailable
+    from = "09/01/2021";
+    to = "09/30/2021";
+    fromEn = "September 1, 2021";
+    toEn = "September 30, 2021";
+  };
+ 
   loadLocalResource(ContestRules[Strings.getLanguage()]).then(text => setText(text));
   return (
     <SafeAreaView style={GlobalStyles.container}>
@@ -28,7 +52,7 @@ export default function ContestRulesScreen({navigation}) {
         <View style={GlobalStyles.content}>
           <PageTitle title={Strings.common.contestRules} />
           <View style={GlobalStyles.content}>
-            <Autolink text={text} />
+            <Autolink text={Strings.formatString(text, fromEn, toEn, from, to)} />
           </View>
         </View>
       </ScrollView>
