5154861a9cf754e1ac87c93a20bb4b786b905205
Mark Khoo <markdkhoo@gmail.com>
2023-03-16 01:09:29 -0700

    UPDATE topWalkers.js and assets

--- screens/main/topWalkers.js
+++ screens/main/topWalkers.js
@@ -8,47 +8,53 @@ import {
   Image,
 } from 'react-native';
 import { GlobalStyles, Colors } from '../../styles';
-import { Realm, Strings } from '../../lib';
+import { Api, Realm, Strings } from '../../lib';
+import numeral from 'numeral';
 
 export default function TopWalkersScreen() {
+  const [flyoutState, setFlyoutState] = useState(false);
+  const [uuid, setUUID] = useState('00000000-0000-0000-0000-000000000000');
+  const [user, setUser] = useState({id: '00000000-0000-0000-0000-000000000000', position: 1, steps: 1});
   const [walkers, setWalkers] = useState([]);
-  const [uuid, setUUID] = useState('00000000-0000-0000-0000-000000000000')
-  const [flyoutState, setFlyoutState] = useState(false)
 
   const dummyData = [
-    { id: '7121e191-0b31-43a3-a7f0-000000000010', position: 1, steps: 999590355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000020', position: 2, steps: 71490355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000000', position: 3, steps: 5390355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000001', position: 4, steps: 3290355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000002', position: 5, steps: 1190355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000003', position: 6, steps: 1110355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000004', position: 7, steps: 1100355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000005', position: 8, steps: 1100055 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000006', position: 9, steps: 1100005 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000007', position: 10, steps: 190355 },
-    { id: '7121e191-0b31-43a3-a7f0-2c07bc7da9ab', position: 98, steps: 190255 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000009', position: 99, steps: 190155 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000060', position: 100, steps: 90055 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000070', position: 101, steps: 90005 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000080', position: 102, steps: 90000 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000090', position: 103, steps: 80355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000700', position: 9998, steps: 9035 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000800', position: 9999, steps: 8355 },
-    { id: '7121e191-0b31-43a3-a7f0-000000000900', position: 10000, steps: 935 },
-    { id: '7121e191-0b31-43a3-a7f0-000000007000', position: 10001, steps: 355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000001', position: 1, steps: 999590355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000002', position: 2, steps: 71490355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000003', position: 3, steps: 5390355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000004', position: 4, steps: 3290355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000005', position: 5, steps: 1190355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000006', position: 6, steps: 1110355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000007', position: 7, steps: 1100355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000008', position: 8, steps: 1100055 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000009', position: 9, steps: 1100005 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000010', position: 10, steps: 190355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000011', position: 98, steps: 190255 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000012', position: 99, steps: 190155 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000013', position: 100, steps: 90055 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000014', position: 101, steps: 90005 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000015', position: 500, steps: 90000 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000016', position: 800, steps: 80355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000017', position: 9998, steps: 9035 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000018', position: 9999, steps: 8355 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000019', position: 10000, steps: 935 },
+    { id: '7121e191-0b31-43a3-a7f0-000000000020', position: 88888, steps: 355 },
   ];
 
   useEffect(async () => {
-    setUUID((await Realm.getUser()).id);
+    Realm.getUser().then(user => {
+      setUUID(user.id);
+      return user.id
+    }).then(id => {
 
-    setWalkers(dummyData);
-    const userData = walkers.find(o => o.id === uuid);
-    if (userData && userData.position > 10) setFlyoutState(true);
+      // Api
+      dummyData[Math.floor(Math.random()*dummyData.length)].id = id;
+      setWalkers(dummyData);
+
+      const userData = dummyData.find(o => o.id === id);
+      setUser(userData);
+      if (userData && userData.position > 10) setFlyoutState(true);
+    })
   }, []);
-
-  const addCommas = (num) => {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
 
   const positionFontSize = (num) => {
     const baseFontSize = 30;
@@ -58,42 +64,49 @@ export default function TopWalkersScreen() {
     return (baseFontSize - ((numLength - 1) * fontSizeMultiplier));
   }
 
+  const positionCard = (participant, userId, additionalStyles = {}) => {
+    return(
+      <View
+        key={participant.position}
+        style={[styles.walkerContainer, additionalStyles]}
+      >
+        <View style={styles.walkerIndex}>
+          <View style={styles.walkerIndexBorder}>
+            <Text style={[styles.walkerIndexFont, { fontSize: positionFontSize(participant.position) }]}>
+              {participant.position}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.walkerText}>
+          <Text style={styles.walkerPosition}>
+            {`${participant.id === userId? "This is You!" : `Walker #${numeral(participant.position).format('0,0')}`}`}
+          </Text>
+          <Text style={styles.walkerScore}>
+            {numeral(participant.steps).format('0,0')}
+          </Text>
+        </View>
+      </View>
+    )
+  }
+
   return (
     <SafeAreaView style={[GlobalStyles.container, styles.background]}>
       <ScrollView>
         <View style={GlobalStyles.content}>
           <View style={[styles.pageTitle]}>
             <Image
-              style={{ resizeMode: 'contain', width: `${25.75}%` }}
-              source={require('../../assets/top-walkers-trophy.png')}
+              style={{ resizeMode: 'contain', width: `${24}%` }}
+              source={require('../../assets/top_walkers_trophy.png')}
             />
             <Image
-              style={{ resizeMode: 'contain', width: `${74.25}%` }}
-              source={require('../../assets/top-walkers.png')}
+              style={{ resizeMode: 'contain', width: `${72}%`, marginLeft: 16 }}
+              source={require('../../assets/top_walkers.png')}
             />
           </View>
 
-          {walkers.map((participant, index) => {
-            return (
-              <View
-                key={participant.position}
-                style={[styles.walkerContainer, (uuid === participant.id ? { backgroundColor: Colors.accent.teal } : {})]}
-              >
-                <View style={styles.walkerIndex}>
-                  <View style={styles.walkerIndexBorder}>
-                    <Text style={[styles.walkerIndexFont, { fontSize: positionFontSize(participant.position) }]}>
-                      {participant.position}
-                    </Text>
-                  </View>
-                </View>
-                <Text style={styles.walkerText}>
-                  {`Walker #${addCommas(participant.position)}`}
-                </Text>
-                <Text style={styles.walkerScore}>
-                  {addCommas(participant.steps)}
-                </Text>
-              </View>
-            )
+          {walkers.map((participant) => {
+            const additionalStyles = (uuid === participant.id ? { backgroundColor: Colors.accent.teal } : {})
+            return positionCard(participant, uuid, additionalStyles);
           })}
 
           {flyoutState ?
@@ -106,21 +119,7 @@ export default function TopWalkersScreen() {
       </ScrollView>
 
       {flyoutState ?
-        <View style={[styles.walkerContainer, { backgroundColor: Colors.accent.teal, position: 'absolute', bottom: 0, marginLeft: 16, marginRight: 16 }]}>
-          <View style={styles.walkerIndex}>
-            <View style={styles.walkerIndexBorder}>
-              <Text style={[styles.walkerIndexFont, { fontSize: positionFontSize(3) }]}>
-                {3}
-              </Text>
-            </View>
-          </View>
-          <Text style={styles.walkerText}>
-            {`Walker #${addCommas(3)}`}
-          </Text>
-          <Text style={styles.walkerScore}>
-            {addCommas(5390355)}
-          </Text>
-        </View>
+        positionCard(user, uuid, styles.flyout)
         : <></>}
 
     </SafeAreaView>
@@ -169,33 +168,35 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    overflow: 'hidden'
   },
   walkerIndexFont: {
     ...GlobalStyles.boxShadow,
     color: Colors.secondary.blue,
     fontWeight: 'bold',
-    overflow: 'hidden'
+    overflow: 'hidden',
+    position: "absolute"
   },
   walkerText: {
+    width: '76%',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    paddingRight: 16,
+  },
+  walkerPosition: {
     ...GlobalStyles.h2,
     color: Colors.accent.deepPurple,
-    textAlign: 'left',
-    height: 'auto',
-    width: '39.5%',
-    overflow: 'hidden',
-    flexDirection: 'row',
-    flexWrap: 'nowrap'
   },
   walkerScore: {
     ...GlobalStyles.h2,
     color: Colors.accent.deepPurple,
-    textAlign: 'left',
-    paddingRight: 16,
-    height: 'auto',
-    width: '36.5%',
-    textAlign: "right",
-    overflow: 'hidden',
-    flexDirection: 'row',
-    flexWrap: 'nowrap'
   },
+  flyout: {
+    backgroundColor: Colors.accent.teal,
+    position: 'absolute',
+    bottom: 0,
+    marginLeft: 16,
+    marginRight: 16
+  }
 });
